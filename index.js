bf3458abf60f43246fa618c517c8c017110aa36e
Devin Jones <devin.jones21083@gmail.com>
2023-04-02 23:35:57 -0400

    Edited index.js

--- index.js
+++ index.js
@@ -12,5 +12,45 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const updatedTutorials = tutorials.map(function (title) {
+    debugger; 
+    const splitTitle = title.split(" ");
+    debugger;
+    const upperSplit = splitTitle.map(word => (word.charAt(0).toUpperCase() + word.slice(1)));
+    debugger;
+    let newElementVal = "";
+    debugger;
+    for (let wordIndex = 0; wordIndex < (upperSplit.length -1); wordIndex++) {
+      debugger;  
+      newElementVal = newElementVal + upperSplit[wordIndex] + " ";
+    }
+    debugger;
+    newElementVal = newElementVal + upperSplit[upperSplit.length -1];
+    return newElementVal;
+  });
+
+      
+  debugger;
+  
+  
+    //for (const tutorial of tutorials) {
+     // const splitTitle = tutorial.split(" ");
+     // for (let wordIndex = 0; wordIndex < splitTitle.length; wordIndex++) {
+      
+      //}
+
+    //  }
+    /*
+    for (let letterIndex = 0; letterIndex < title.length; letterIndex++) {
+      
+      if(letterIndex === 0) {
+        newArraY = title.map()
+      } else { 
+        if(tutorial.charAt(letterIndex - 1) === " ") {
+        tutorial.map(word => word.charAt(letterIndex).toUpperCase());
+        }
+      }
+    }*/
+    return updatedTutorials;
 }
+titleCased();
